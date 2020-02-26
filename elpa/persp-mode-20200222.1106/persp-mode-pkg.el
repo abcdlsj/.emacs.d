cd727fa86a84a383687400a665bd1c9dce53b966
abcdlsj <lisongjianshuai@gmail.com>
2020-02-26 17:34:10 +0800

    change

--- elpa/persp-mode-20200222.1106/persp-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/abcdlsj/.emacs.d/elpa/persp-mode-20200222.1106/persp-mode.el  -*- no-byte-compile: t -*-
-(define-package "persp-mode" "20200222.1106" "windows/buffers sets shared among frames + save/load." 'nil :commit "391a7dc248c9c04b7ad424c696bdff578e14dd2c" :keywords '("perspectives" "session" "workspace" "persistence" "windows" "buffers" "convenience") :authors '(("Constantin Kulikov (Bad_ptr)" . "zxnotdead@gmail.com")) :maintainer '("Constantin Kulikov (Bad_ptr)" . "zxnotdead@gmail.com") :url "https://github.com/Bad-ptr/persp-mode.el")
--- elpa/visual-fill-column-20190422.2154/visual-fill-column-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/abcdlsj/.emacs.d/elpa/visual-fill-column-20190422.2154/visual-fill-column.el  -*- no-byte-compile: t -*-
-(define-package "visual-fill-column" "20190422.2154" "fill-column for visual-line-mode" '((emacs "24.3")) :commit "9a3a2ef3c7c76666a0c9271a821256eec9daab2a" :authors '(("Joost Kremers" . "joostkremers@fastmail.fm")) :maintainer '("Joost Kremers" . "joostkremers@fastmail.fm"))
