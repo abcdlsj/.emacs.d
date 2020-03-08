cf165d964c8515f149bf7e00d50aad0310f2474c
abcdlsj <lisongjianshuai@gmail.com>
2020-03-08 09:54:50 +0800

    try to change c-basic-offset 4 but I don't do this

--- core/init-lang.el
+++ core/init-lang.el
@@ -1,7 +1,7 @@
 (use-package google-c-style
-:hook
-(c-mode-common-hook . google-set-c-style)
-(c-mode-common-hook . google-make-newline-indent))
+  :hook
+  (c-mode-common-hook . google-set-c-style)
+  (c-mode-common-hook . google-make-newline-indent))
 
 ;; (require 'clang-format)
 ;; (setq clang-format-style-option "google")
